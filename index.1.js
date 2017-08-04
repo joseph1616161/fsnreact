a90fcf938589868e47855dbcf00fb0869c869abd
Kayne James <kaynejames@Kaynes-MacBook-Pro.local>
2017-08-04 16:24:04 +0800

    added Oauth

--- index.1.js
+++ index.1.js
@@ -1,18 +1,15 @@
 const express = require("express");
+const app = express();
+const keys = require("./config/keys");
 const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
-const keys = require("./config/keys");
-
-require("./models/user");
+const cookieSession = require("cookie-session");
+require("./models/user"); //order of require can result in error
 require("./services/passport");
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/emaily");
-
-const app = express();
-app.disable("x-powered-by");
 app.use(bodyParser.json());
 app.use(
 	cookieSession({
@@ -20,10 +17,10 @@ app.use(
 		keys: [keys.cookieKey]
 	})
 );
+
+//tell passport to make use of cookie to do the authentication
 app.use(passport.initialize());
 app.use(passport.session());
-
-require("./routes/authRoutes")(app);
 
 app.get("/", function(req, res) {
 	res.send("hi");
